3793d66e86c4ab1b83d35c2558eb876b2f8e4f11
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-26 14:16:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  document.querySelector('selector');
+}
 
+function nestedTarget() {
+  targetSpot = document.getElementById("nested").children[0].children[0].children[0].children[0];
+  targetSpot.innerHTML;
+}
+
+function increaseRankBy(n) {
+  lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  div = document.getElementById('grand-node')
+  nextChild = div.children[0]
+    while (nextChild !== undefined) {
+      nextChild = nextChild.children[0];
+      console.log(nextChild.innerHTML);
+    }
+}
