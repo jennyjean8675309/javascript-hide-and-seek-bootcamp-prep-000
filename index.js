c1bf6be8f3b0da2a125df4b5c2db8f24146da315
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-26 13:04:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  document.querySelector('selector');
+}
 
+function nestedTarget() {
+  targetSpot = document.getElementById("nested").children[0].children[0].children[0].children[0];
+  targetSpot;
+}
+
+function increaseRankBy(n) {
+  lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  div = document.getElementById('grand-node')
+  
+}
